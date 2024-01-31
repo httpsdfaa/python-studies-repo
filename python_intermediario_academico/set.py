d47ccc82faa0912ccbae0152692f578f81df6619
httpsdfaa <deivisondfaa@gmail.com>
2024-01-30 22:30:35 -0300

    Getting started abour sets

---
+++ python_intermediario_academico/set.py
@@ -0,0 +1,15 @@
+"""
+    Sets - Conjuntos em Python (Type set)
+    Conjuntos são ensinados na matemática
+
+    Representados graficamente pelo o diagrama de Venn
+    Sets em Python são mutáveis, porém aceitam apenas tipos imutáveis como valor interno
+"""
+
+# CRIANDO UM SET
+# UM SET NÃO É UM DICT (DICIONÁRIO)
+s1 = set()
+print(type(s1))
+
+s2 = set('Guido')
+print(s2) # iterável
